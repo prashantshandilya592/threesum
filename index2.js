b0274e4790df98aa317ac809178ac216939432aa
PRASHANT SHANDILYA <30324628+prashantshandilya592@users.noreply.github.com>
2021-12-05 13:08:46 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,16 @@
+console.log("sum of three function parameter");
+
+let sum=function(a){
+    let sum2=function(b){
+        let sum3=function(c){
+
+
+        let result=a+b+c;
+        console.log(result);
+    }
+    return sum3;
+    }
+    return sum2;
+}
+
+sum(30)(40)(50);
